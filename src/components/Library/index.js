b86dc2c2f8998855641a4504db52dca5ce06828e
nzwithee@gmail.com <Cartoon#01>
2022-06-29 18:43:23 -0500

    initial updates

---
+++ src/components/Library/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+function Library() {
+    return (
+    <div class="wrapper">
+    <header class="col-12">
+        <h1 class="title text-center">D&D Character Library</h1>
+    </header>
+    <main>
+    </main>
+    </div>
+    )
+}
+
+export default Library;
