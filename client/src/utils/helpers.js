a62b730af0bba22843582b24f6397804311f662a
soulslurpee <charles.d.thompson87@gmail.com>
2022-07-11 15:26:01 -0400

    added beta score calculations for ability and skill mods

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -6,3 +6,16 @@ export function capitalizeFirstLetter(string) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
+
+export function abilityMod (score) {
+    return Math.floor((score - 10) / 2);
+}
+
+export function skillMod (score, prof) {
+  if (prof) {
+    return Math.floor((score - 10) / 2) + prof;
+  } else {
+    return Math.floor((score - 10) / 2);
+  }
+};
+
