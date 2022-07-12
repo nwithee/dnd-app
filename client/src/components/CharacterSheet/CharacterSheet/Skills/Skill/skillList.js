7a0dd1acd15e190b196d4aa2dbe60344eba73198
soulslurpee <charles.d.thompson87@gmail.com>
2022-07-12 14:16:08 -0400

    Reverting to previous version
    Revert "Bio, Ability Scores, & Skills basically functional, no styling"
    
    This reverts commit 60dfe0aa99e0a7e91f3de9c81e38e01d7f36a6b1.

--- client/src/components/CharacterSheet/CharacterSheet/Skills/Skill/skillList.js
+++
@@ -1,3 +0,0 @@
-const skillList = ['Acrobatics', 'Animal Handling', 'Arcana', 'Athletics', 'Deception', 'History', 'Insight', 'Intimidation', 'Investigation', 'Medicine', 'Nature', 'Perception', 'Performance', 'Persuasion', 'Religion', 'Sleight of Hand', 'Stealth', 'Survival'];
-
-export default skillList;
