30aaf16980cd5050a7be35648455f1009edab398
Ansab Sidiki <ansab@sidiki.io>
2022-07-13 12:18:59 -0400

    concurrently working

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const db = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +22,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
