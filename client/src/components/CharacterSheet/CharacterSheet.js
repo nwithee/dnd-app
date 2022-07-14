0a8759960d50e2832f4b9a98242b265c428e32db
soulslurpee <charles.d.thompson87@gmail.com>
2022-07-14 17:48:46 -0400

    added all remaing states for each DB variable

--- client/src/components/CharacterSheet/CharacterSheet.js
+++ client/src/components/CharacterSheet/CharacterSheet.js
@@ -18,6 +18,42 @@ const CharacterSheet = () => {
   const [playerbackground, setPlayerbackground] = useState("");
   const [playeralignment, setPlayeralignment] = useState("");
   const [playerlevel, setPlayerlevel] = useState("");
+  const [acrobatics, setAcrobatics] = useState(0);
+  const [animalHandling, setAnimalHandling] = useState(0);
+  const [arcana, setArcana] = useState(0);
+  const [athletics, setAthletics] = useState(0);
+  const [deception, setDeception] = useState(0);
+  const [history, setHistory] = useState(0);
+  const [insight, setInsight] = useState(0);
+  const [intimidation, setIntimidation] = useState(0);
+  const [investigation, setInvestigation] = useState(0);
+  const [medicine, setMedicine] = useState(0);
+  const [nature, setNature] = useState(0);
+  const [perception, setPerception] = useState(0);
+  const [performance, setPerformance] = useState(0);
+  const [persuasion, setPersuasion] = useState(0);
+  const [religion, setReligion] = useState(0);
+  const [sleightOfHand, setSleightOfHand] = useState(0);
+  const [stealth, setStealth] = useState(0);
+  const [survival, setSurvival] = useState(0);
+  const [experiencePoints, setExperiencePoints] = useState(0);
+  const [currentHitPoints, setCurrentHitPoints] = useState(0);
+  const [tempHitPoints, setTempHitPoints] = useState(0);
+  const [armorClass, setArmorClass] = useState(0);
+  const [initiative, setInitiative] = useState(0);
+  const [speed, setSpeed] = useState(0);
+  const [personailtyTraits, setPersonalityTraits] = useState(0);
+  const [ideals, setIdeals] = useState(0);
+  const [bonds, setBonds] = useState(0);
+  const [flaws, setFlaws] = useState(0);
+  const [proficiencyBonus, setProficiencyBonus] = useState(0);
+  const [inspiration, setInspiration] = useState(0);
+
+
+
+
+
+
 
   const [addChar, { error }] = useMutation(ADD_CHAR);
 
@@ -25,7 +61,8 @@ const CharacterSheet = () => {
     addChar({  
     variables: {
       charName: charname,
-      classNlevel: playerclass,
+      classNlevel: playerclass + " " + playerlevel,
+      playerName: playername,
       race: playerrace,
       background: playerbackground,
       alignment: playeralignment,
@@ -35,6 +72,36 @@ const CharacterSheet = () => {
       intelligence: intelligence,
       wisdom: wisdom,
       charisma: charisma,
+      acrobatics: acrobatics,
+      animalHandling: animalHandling,
+      arcana: arcana,
+      athletics: athletics,
+      deception: deception,
+      history: history,
+      insight: insight,
+      intimidation: intimidation,
+      investigation: investigation,
+      medicine: medicine,
+      nature: nature,
+      perception: perception,
+      performance: performance,
+      persuasion: persuasion,
+      religion: religion,
+      sleightOfHand: sleightOfHand,
+      stealth: stealth,
+      survival: survival,
+      experiencePoints: experiencePoints,
+      currentHitPoints: currentHitPoints,
+      tempHitPoints: tempHitPoints,
+      armorClass: armorClass,
+      initiative: initiative,
+      speed: speed,
+      personalityTraits: personalityTraits,
+      ideals: ideals,
+      bonds: bonds,
+      flaws: flaws,
+      proficiencyBonus: proficiencyBonus,
+      inspiration: inspiration,
     }
   })
   if (error) {
@@ -81,6 +148,99 @@ const CharacterSheet = () => {
         case "charisma":
           setCharisma(value);
           break;
+        case "acrobatics":
+          setAcrobatics(value);
+          break;
+        case "animalHandling":
+          setAnimalHandling(value);
+          break;
+        case "arcana":
+          setArcana(value);
+          break;
+        case "athletics":
+          setAthletics(value);
+          break;
+        case "deception":
+          setDeception(value);
+          break;
+        case "history":
+          setHistory(value);
+          break;
+        case "insight":
+          setInsight(value);
+          break;
+        case "intimidation":
+          setIntimidation(value);
+          break;
+        case "investigation":
+          setInvestigation(value);
+          break;
+        case "medicine":
+          setMedicine(value);
+          break;
+        case "nature":
+          setNature(value);
+          break;
+        case "perception":
+          setPerception(value);
+          break;
+        case "performance":
+          setPerformance(value);
+          break;
+        case "persuasion":
+          setPersuasion(value);
+          break;
+        case "religion":
+          setReligion(value);
+          break;
+        case "sleightOfHand":
+          setSleightOfHand(value);
+          break;
+        case "stealth":
+          setStealth(value);
+          break;
+        case "survival":
+          setSurvival(value);
+          break;
+        case "experiencePoints":
+          setExperiencePoints(value);
+          break;
+        case "playername": 
+          setPlayername(value);
+          break;
+        case "currentHitPoints":
+          setCurrentHitPoints(value);
+          break;
+        case "tempHitPoints":
+          setTempHitPoints(value);
+          break;
+        case "armorClass":
+          setArmorClass(value);
+          break;
+        case "initiative":
+          setInitiative(value);
+          break;
+        case "speed":
+          setSpeed(value);
+          break;
+        case "personalityTraits":
+          setPersonalityTraits(value);
+          break;
+        case "ideals":
+          setIdeals(value);
+          break;
+        case "bonds":
+          setBonds(value);
+          break;
+        case "flaws":
+          setFlaws(value);
+          break;
+        case "proficiencyBonus":
+          setProficiencyBonus(value);
+          break;
+        case "inspiration":
+          setInspiration(value);
+          break;
       default:
         break;
     }
@@ -117,7 +277,7 @@ const CharacterSheet = () => {
         </li>
         <li>
           <label htmlFor="experiencepoints">Experience Points</label>
-          <input name="experiencepoints" placeholder="+0" />
+          <input name="experiencepoints" placeholder="+0" onChange = { handleInputChange }/>
         </li>
       </ul>
     </section>
@@ -194,39 +354,39 @@ const CharacterSheet = () => {
             <div className="label-container">
               <label htmlFor="proficiencybonus">Proficiency Bonus</label>
             </div>
-            <input name="proficiencybonus" placeholder="+2" />
+            <input name="proficiencybonus" placeholder="+2" onChange = { handleInputChange }/>
           </div>
           <div className="saves list-section box">
             <ul>
               <li>
                 <label htmlFor="Strength-save">Strength</label>
-                <input name="Strength-save" placeholder="+0" type="text" />
-                <input name="Strength-save-prof" type="checkbox" />
+                <input name="Strength-save" placeholder="+0" type="text" onChange = { handleInputChange } />
+                <input name="Strength-save-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Dexterity-save">Dexterity</label>
-                <input name="Dexterity-save" placeholder="+0" type="text" />
-                <input name="Dexterity-save-prof" type="checkbox" />
+                <input name="Dexterity-save" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Dexterity-save-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Constitution-save">Constitution</label>
-                <input name="Constitution-save" placeholder="+0" type="text" />
-                <input name="Constitution-save-prof" type="checkbox" />
+                <input name="Constitution-save" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Constitution-save-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Wisdom-save">Wisdom</label>
-                <input name="Wisdom-save" placeholder="+0" type="text" />
-                <input name="Wisdom-save-prof" type="checkbox" />
+                <input name="Wisdom-save" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Wisdom-save-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Intelligence-save">Intelligence</label>
-                <input name="Intelligence-save" placeholder="+0" type="text" />
-                <input name="Intelligence-save-prof" type="checkbox" />
+                <input name="Intelligence-save" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Intelligence-save-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Charisma-save">Charisma</label>
-                <input name="Charisma-save" placeholder="+0" type="text" />
-                <input name="Charisma-save-prof" type="checkbox" />
+                <input name="Charisma-save" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Charisma-save-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
             </ul>
             <div className="label">Saving Throws</div>
@@ -237,127 +397,127 @@ const CharacterSheet = () => {
                 <label htmlFor="Acrobatics">
                   Acrobatics <span className="skill">(Dex)</span>
                 </label>
-                <input name="Acrobatics" placeholder="+0" type="text" />
-                <input name="Acrobatics-prof" type="checkbox" />
+                <input name="Acrobatics" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Acrobatics-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Animal Handling">
                   Animal Handling <span className="skill">(Wis)</span>
                 </label>
-                <input name="Animal Handling" placeholder="+0" type="text" />
-                <input name="Animal Handling-prof" type="checkbox" />
+                <input name="Animal Handling" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Animal Handling-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Arcana">
                   Arcana <span className="skill">(Int)</span>
                 </label>
-                <input name="Arcana" placeholder="+0" type="text" />
-                <input name="Arcana-prof" type="checkbox" />
+                <input name="Arcana" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Arcana-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Athletics">
                   Athletics <span className="skill">(Str)</span>
                 </label>
-                <input name="Athletics" placeholder="+0" type="text" />
-                <input name="Athletics-prof" type="checkbox" />
+                <input name="Athletics" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Athletics-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Deception">
                   Deception <span className="skill">(Cha)</span>
                 </label>
-                <input name="Deception" placeholder="+0" type="text" />
-                <input name="Deception-prof" type="checkbox" />
+                <input name="Deception" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Deception-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="History">
                   History <span className="skill">(Int)</span>
                 </label>
-                <input name="History" placeholder="+0" type="text" />
-                <input name="History-prof" type="checkbox" />
+                <input name="History" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="History-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Insight">
                   Insight <span className="skill">(Wis)</span>
                 </label>
-                <input name="Insight" placeholder="+0" type="text" />
-                <input name="Insight-prof" type="checkbox" />
+                <input name="Insight" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Insight-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Intimidation">
                   Intimidation <span className="skill">(Cha)</span>
                 </label>
-                <input name="Intimidation" placeholder="+0" type="text" />
-                <input name="Intimidation-prof" type="checkbox" />
+                <input name="Intimidation" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Intimidation-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Investigation">
                   Investigation <span className="skill">(Int)</span>
                 </label>
-                <input name="Investigation" placeholder="+0" type="text" />
-                <input name="Investigation-prof" type="checkbox" />
+                <input name="Investigation" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Investigation-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Medicine">
                   Medicine <span className="skill">(Wis)</span>
                 </label>
-                <input name="Medicine" placeholder="+0" type="text" />
-                <input name="Medicine-prof" type="checkbox" />
+                <input name="Medicine" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Medicine-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Nature">
                   Nature <span className="skill">(Int)</span>
                 </label>
-                <input name="Nature" placeholder="+0" type="text" />
-                <input name="Nature-prof" type="checkbox" />
+                <input name="Nature" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Nature-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Perception">
                   Perception <span className="skill">(Wis)</span>
                 </label>
-                <input name="Perception" placeholder="+0" type="text" />
-                <input name="Perception-prof" type="checkbox" />
+                <input name="Perception" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Perception-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Performance">
                   Performance <span className="skill">(Cha)</span>
                 </label>
-                <input name="Performance" placeholder="+0" type="text" />
-                <input name="Performance-prof" type="checkbox" />
+                <input name="Performance" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Performance-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Persuasion">
                   Persuasion <span className="skill">(Cha)</span>
                 </label>
-                <input name="Persuasion" placeholder="+0" type="text" />
-                <input name="Persuasion-prof" type="checkbox" />
+                <input name="Persuasion" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Persuasion-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Religion">
                   Religion <span className="skill">(Int)</span>
                 </label>
-                <input name="Religion" placeholder="+0" type="text" />
-                <input name="Religion-prof" type="checkbox" />
+                <input name="Religion" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Religion-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Sleight of Hand">
                   Sleight of Hand <span className="skill">(Dex)</span>
                 </label>
-                <input name="Sleight of Hand" placeholder="+0" type="text" />
-                <input name="Sleight of Hand-prof" type="checkbox" />
+                <input name="Sleight of Hand" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Sleight of Hand-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Stealth">
                   Stealth <span className="skill">(Dex)</span>
                 </label>
-                <input name="Stealth" placeholder="+0" type="text" />
-                <input name="Stealth-prof" type="checkbox" />
+                <input name="Stealth" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Stealth-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
               <li>
                 <label htmlFor="Survival">
                   Survival <span className="skill">(Wis)</span>
                 </label>
-                <input name="Survival" placeholder="+0" type="text" />
-                <input name="Survival-prof" type="checkbox" />
+                <input name="Survival" placeholder="+0" type="text" onChange = { handleInputChange }/>
+                <input name="Survival-prof" type="checkbox" onChange = { handleInputChange }/>
               </li>
             </ul>
             <div className="label">Skills</div>
